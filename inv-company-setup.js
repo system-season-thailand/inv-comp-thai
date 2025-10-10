02a3007b219cda2226d0690d5967a8433d7d440c
Bandar-Zuhair <my.github.websites@gmail.com>
2025-10-10 22:22:49 +0700

    .

--- inv-company-setup.js
+++ inv-company-setup.js
@@ -1161,18 +1161,31 @@ function processInvoiceData(data) {
         const top_left_inv_company_orignal_div_id = document.getElementById("top_left_inv_company_orignal_div_id");
         const top_left_inv_company_golden_div_id = document.getElementById("top_left_inv_company_golden_div_id");
         const invoice_company_golden_under_guest_name_info_div = document.getElementById("invoice_company_golden_under_guest_name_info_div");
+        const invoice_company_al_ghazali_under_guest_name_info_div = document.getElementById("invoice_company_al_ghazali_under_guest_name_info_div");
 
 
 
         // Determine the orignal styling or the golden travel
         if (agencyUpper.includes("GOLDEN TRAVEL")) {
+
             top_left_inv_company_orignal_div_id.style.display = "none";
             top_left_inv_company_golden_div_id.style.display = "flex";
             invoice_company_golden_under_guest_name_info_div.style.display = "block";
+            invoice_company_al_ghazali_under_guest_name_info_div.style.display = "none";
+
+        } else if (agencyUpper.includes("AL GHAZALI") || agencyUpper.includes("ALGHAZALI")) {
+
+            top_left_inv_company_orignal_div_id.style.display = "none";
+            top_left_inv_company_golden_div_id.style.display = "flex";
+            invoice_company_golden_under_guest_name_info_div.style.display = "none";
+            invoice_company_al_ghazali_under_guest_name_info_div.style.display = "block";
+
         } else {
+
             top_left_inv_company_orignal_div_id.style.display = "flex";
             top_left_inv_company_golden_div_id.style.display = "none";
             invoice_company_golden_under_guest_name_info_div.style.display = "none";
+            invoice_company_al_ghazali_under_guest_name_info_div.style.display = "none";
         }
 
 
