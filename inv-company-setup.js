464a5657095ed6d260c94711a33e7e0ae97db6a3
Bandar-Zuhair <my.github.websites@gmail.com>
2025-10-11 16:26:44 +0700

    Update inv-company-setup.js

--- inv-company-setup.js
+++ inv-company-setup.js
@@ -1171,21 +1171,38 @@ function processInvoiceData(data) {
             top_left_inv_company_orignal_div_id.style.display = "none";
             top_left_inv_company_golden_div_id.style.display = "flex";
             invoice_company_golden_under_guest_name_info_div.style.display = "block";
-            invoice_company_al_ghazali_under_guest_name_info_div.style.display = "none";
+
+
+            /* in 11 Oct 2026 delete the following if condition (I used it tp avoid error in old inv) */
+            if (invoice_company_al_ghazali_under_guest_name_info_div) {
+                invoice_company_al_ghazali_under_guest_name_info_div.style.display = "none";
+            }
+
 
         } else if (agencyUpper.includes("AL GHAZALI") || agencyUpper.includes("ALGHAZALI")) {
 
             top_left_inv_company_orignal_div_id.style.display = "none";
             top_left_inv_company_golden_div_id.style.display = "flex";
             invoice_company_golden_under_guest_name_info_div.style.display = "none";
-            invoice_company_al_ghazali_under_guest_name_info_div.style.display = "block";
+
+
+            /* in 11 Oct 2026 delete the following if condition (I used it tp avoid error in old inv) */
+            if (invoice_company_al_ghazali_under_guest_name_info_div) {
+                invoice_company_al_ghazali_under_guest_name_info_div.style.display = "block";
+            }
+
 
         } else {
 
             top_left_inv_company_orignal_div_id.style.display = "flex";
             top_left_inv_company_golden_div_id.style.display = "none";
             invoice_company_golden_under_guest_name_info_div.style.display = "none";
-            invoice_company_al_ghazali_under_guest_name_info_div.style.display = "none";
+
+
+            /* in 11 Oct 2026 delete the following if condition (I used it tp avoid error in old inv) */
+            if (invoice_company_al_ghazali_under_guest_name_info_div) {
+                invoice_company_al_ghazali_under_guest_name_info_div.style.display = "none";
+            }
         }
 
 
